6e50122e479b80656cf00f7f99a76b19cd68008c
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2024-01-17 00:27:27 +0200

    Update post-details.js

--- post-details.js
+++ post-details.js
@@ -4,12 +4,14 @@ const postId = urlParams.get('postId');
 async function getPostDetails() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     const postDetails = await response.json();
+    console.log(response)
     return postDetails;
 }
 
 async function getPostComments() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
     const comments = await response.json();
+    console.log(comments)
     return comments;
 }
 
@@ -19,8 +21,11 @@ async function displayPostDetails() {
 
     const postDetailsDiv = document.getElementById('post-details');
     postDetailsDiv.innerHTML = `
+        <p>Post Details:</p>
         <p>Title: ${postDetails.title}</p>
         <p>Body: ${postDetails.body}</p>
+        <p>User ID: ${postDetails.userId}</p>
+        <p>Post ID: ${postId}</p>
     `;
 
     const commentsDiv = document.getElementById('comments');
@@ -30,7 +35,9 @@ async function displayPostDetails() {
         commentBlock.innerHTML = `
             <p>Name: ${comment.name}</p>
             <p>Email: ${comment.email}</p>
-            <p>Comment: ${comment.body}</p>
+            <p>Comment ID: ${comment.id}</p>
+            <p>Body: ${comment.body}</p>
+            <p>Post ID: ${postId}</p>
         `;
         commentsDiv.appendChild(commentBlock);
     });
