647ee3f42fee0f095daca66a711cb103f8729f31
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2024-01-16 15:25:20 +0200

    Update user-details.js

--- user-details.js
+++ user-details.js
@@ -14,8 +14,29 @@ async function displayUserDetails() {
     userDetailsDiv.innerHTML = `
         <p>ID: ${userDetails.id}</p>
         <p>Name: ${userDetails.name}</p>
+        <p>Username: ${userDetails.username}</p>
+        <p>Email: ${userDetails.email}</p>
+        <p>Address:</p>
+        
+            <li>Street: ${userDetails.address.street}</li>
+            <li>Suite: ${userDetails.address.suite}</li>
+            <li>City: ${userDetails.address.city}</li>
+            <li>Zipcode: ${userDetails.address.zipcode}</li>
+        
+        <p>Geo:</p>
+        
+        <li>lat: ${userDetails.address.geo.lat}</li>   
+        <li>Ing: ${userDetails.address.geo.lng}</li>      
+        <p>Phone: ${userDetails.phone}</p>
+        <p>Website: ${userDetails.website}</p>
+        <p>Company:</p>
+        <li>name:${userDetails.company.name}</li>
+        <li>catchPhrase:${userDetails.company.catchPhrase}</li>
+        <li>bs:${userDetails.company.bs}</li>
     `;
 }
+
+
 
 function showUserPosts() {
     window.location.href = `user-posts.html?userId=${userId}`;
