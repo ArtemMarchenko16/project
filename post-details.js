98387943f3cfffa0da86f1046b56aa6ed44e73f4
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2024-01-17 11:56:04 +0200

    Update post-details.js

--- post-details.js
+++ post-details.js
@@ -4,14 +4,12 @@ const postId = urlParams.get('postId');
 async function getPostDetails() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     const postDetails = await response.json();
-    console.log(response)
     return postDetails;
 }
 
 async function getPostComments() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
     const comments = await response.json();
-    console.log(comments)
     return comments;
 }
 
